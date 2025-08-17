643c8a0aeb3a6899e69e41f8ccd491f70e82b198
Satyam-Rk <satyamrk2014@gmail.com>
2025-08-17 15:40:35 +0530

    Property binding, Loop & data between components

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PropertyCardComponent } from "./property/property-card/property-card.component";
-import { PropertyList } from './property/property-list/property-list.component';
+// import { PropertyCardComponent } from "./property/property-card/property-card.component";
+import { PropertyListComponenet } from './property/property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [PropertyCardComponent, PropertyList, NavBarComponent],
+  imports: [PropertyListComponenet, NavBarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PropertyCardComponent } from "../property-card/property-card.component";
 
 @Component({
   selector: 'app-property-list',
-  imports: [],
   templateUrl: './property-list.component.html',
-  styleUrl: './property-list.component.css'
+  styleUrl: './property-list.component.css',
+  imports: [PropertyCardComponent]
 })
-export class PropertyList {
 
+export class PropertyListComponenet implements OnInit {
+   Properties: Array<any> = [
+    {
+        "Id": 1,
+        "Name": "Birla House",
+        "Type": "House",
+        "Price": 12000
+    },
+    {
+        "Id": 2,
+        "Name": "Erose Villa",
+        "Type": "Villa",
+        "Price": 13000
+    },
+    {
+        "Id": 3,
+        "Name": "Mark Hill",
+        "Type": "Flat",
+        "Price": 15000
+    },
+    {
+        "Id": 4,
+        "Name": "Gun Hill",
+        "Type": "Duplex",
+        "Price": 18000
+    },
+    {
+        "Id": 5,
+        "Name": "Macro Home",
+        "Type": "House",
+        "Price": 17000
+    },
+    {
+        "Id": 6,
+        "Name": "Pearl White House",
+        "Type": "House",
+        "Price": 10000
+    }
+  ]
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 }
