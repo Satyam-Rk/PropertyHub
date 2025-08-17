657500d08c60e02dbe4fbcdc466d44befcb06ec1
Satyam-Rk <satyamrk2014@gmail.com>
2025-08-17 18:29:32 +0530

    Added service to get the property data using httpClient.

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PropertyCardComponent } from "../property-card/property-card.component";
+import { HousingService } from '../../services/housing.service';
 
 @Component({
   selector: 'app-property-list',
@@ -9,47 +10,18 @@ import { PropertyCardComponent } from "../property-card/property-card.component"
 })
 
 export class PropertyListComponenet implements OnInit {
-   Properties: Array<any> = [
-    {
-        "Id": 1,
-        "Name": "Birla House",
-        "Type": "House",
-        "Price": 12000
-    },
-    {
-        "Id": 2,
-        "Name": "Erose Villa",
-        "Type": "Villa",
-        "Price": 13000
-    },
-    {
-        "Id": 3,
-        "Name": "Mark Hill",
-        "Type": "Flat",
-        "Price": 15000
-    },
-    {
-        "Id": 4,
-        "Name": "Gun Hill",
-        "Type": "Duplex",
-        "Price": 18000
-    },
-    {
-        "Id": 5,
-        "Name": "Macro Home",
-        "Type": "House",
-        "Price": 17000
-    },
-    {
-        "Id": 6,
-        "Name": "Pearl White House",
-        "Type": "House",
-        "Price": 10000
-    }
-  ]
+   Properties: any = [];
 
-    constructor() { }
+    constructor(private housingService: HousingService) { }
 
     ngOnInit(): void {
+        this.housingService.getAllProperties().subscribe(
+            data => {
+                this.Properties = data;
+                console.log(data);
+            }, error => {
+                console.log(error);
+            }
+        );
     }
 }
