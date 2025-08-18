56b93376a70a5fddf4e7988f269e204dba3d0e52
Satyam-Rk <satyamrk2014@gmail.com>
2025-08-18 15:52:55 +0530

    Reactive forms to register a user, Added register/login componenets, Validation, Custome validation & validation messges.

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
 import { PropertyListComponenet } from './property/property-list/property-list.component';
 import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+import { UserLoginComponent } from './user/user-login/user-login.component';
+import { UserRegisterComponent } from './user/user-register/user-register.component';
 
 export const routes: Routes = [
     { path: '', component: PropertyListComponenet },
     { path: 'add-property', component: AddPropertyComponent },
     { path: 'rent-property', component: PropertyListComponenet },
     { path: 'property-detail/:id', component: PropertyDetailComponent },
+    { path: 'user/login', component: UserLoginComponent},
+    { path: 'user/register', component: UserRegisterComponent },
     { path: '**', component: PropertyListComponenet}
 ];
---
+++ Frontend/src/app/user/user-register/user-register.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-user-register',
+  templateUrl: './user-register.component.html',
+  styleUrls: ['./user-register.component.css'],
+  imports: [ReactiveFormsModule, JsonPipe]
+})
+export class UserRegisterComponent implements OnInit {
+
+  registrationForm!: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.registrationForm = new FormGroup({
+      userName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8), Validators.maxLength(16)]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+      mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
+    }, { validators: passwordMatchingValidatior });
+  }
+
+  get userName() {
+    return this.registrationForm.get('userName') as FormControl;
+  }
+
+  get email() {
+    return this.registrationForm.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.registrationForm.get('password') as FormControl;
+  }
+
+  get confirmPassword() {
+    return this.registrationForm.get('confirmPassword') as FormControl;
+  }
+
+  get mobile() {
+    return this.registrationForm.get('mobile') as FormControl;
+  }
+
+  onRegister() {
+    console.log(this.registrationForm);
+  }
+}
+
+export const passwordMatchingValidatior: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  return password?.value === confirmPassword?.value ? null : { notmatched: true };
+};
