1ef8bf82063b68aa26a6807e03392155cc78cdfb
Satyam-Rk <satyamrk2014@gmail.com>
2025-08-18 13:32:12 +0530

    Template driven form to add property with validations

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-property',
   templateUrl: './add-property.component.html',
-  styleUrls: ['./add-property.component.css']
+  styleUrls: ['./add-property.component.css'],
+  imports: [FormsModule]
 })
 export class AddPropertyComponent implements OnInit {
+  @ViewChild('Form') addPropertyForm!: NgForm;
+  SellRent = '1';
 
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
-  onBack() 
-  {
+  onBack() {
     this.router.navigate(['/']);
+  }
+
+  onSubmit() {
+    console.log('Form submitted');
+    console.log(this.addPropertyForm);
   }
 }
